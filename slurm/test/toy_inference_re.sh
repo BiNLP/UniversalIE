05d39952bafa5be2ff2953e9877764282c37ebe4
BunnyNLP <zhenbinchan@gmail.com>
2025-02-02 16:08:28 +0800

    0202-update

--- slurm/test/toy_inference_re.sh
+++ slurm/test/toy_inference_re.sh
@@ -5,19 +5,19 @@ mkdir -p ${output_dir}
 CUDA_VISIBLE_DEVICES=0,1 python ../../src/inference.py \
     --stage sft \
     --model_name_or_path '/home/chenzhb/Workspaces/LLMs/Meta-Llama-3-8B-Instruct/' \
-    --checkpoint_dir '/home/chenzhb/Workspaces/UniversalIE/lora/llama3-8b-toy/checkpoint-30/' \
+    --checkpoint_dir '/home/chenzhb/Workspaces/UniversalIE/lora/llama3-8b-ALL/checkpoint-2331' \
     --model_name 'llama' \
     --template 'alpaca' \
     --do_predict \
     --input_file '/home/chenzhb/Workspaces/Datasets/UIE/ToyData/train2test_iepile_re.json' \
-    --output_file '/home/chenzhb/Workspaces/UniversalIE/results/RE/ReDocRED/llama3-8b-toy-train.json' \
+    --output_file '/home/chenzhb/Workspaces/UniversalIE/results/RE/ReDocRED/llama3-8b-all-train2test.json' \
     --finetuning_type lora \
     --output_dir 'lora/test' \
     --predict_with_generate \
     --cutoff_len 512 \
     --bf16 \
     --max_new_tokens 300 \
-	  --load_best_model_at_end False
+	--load_best_model_at_end False
 
 
 # python ../../ie2instruction/eval_func.py 
